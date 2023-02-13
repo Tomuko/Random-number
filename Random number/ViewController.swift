7ae7b4165d8ca799f91334b1e63aa163b03cb768
Tomuko <vika_stradzina@mail.ru>
2023-02-13 17:41:39 +0300

    Initial Commit

---
+++ Random number/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Random number
+//
+//  Created by Виктория Страдзина on 13.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
